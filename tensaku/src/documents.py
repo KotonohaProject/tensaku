b96d880fef6bdd3b1a0b6ed58e6d3646566836b3
ShinnosukeUesaka <shinnosuke.uesaka@gmail.com>
2023-08-27 15:56:20 -0700

    delete generate md

--- tensaku/src/documents.py
+++ tensaku/src/documents.py
@@ -8,22 +8,13 @@ from dataclasses import dataclass, field
 
 
 class TensakuDocument(ABC):
-    
-    @abstractmethod
-    def generate_md(self) -> str:
-        pass
-    
-    def generate_html(self) -> str:
-        pass
+    pass
 
 
 @dataclass
 class NativeExplanationDocument(TensakuDocument):
     explanation: str
     exists: bool = True
-
-    def generate_md(self):
-        return self.explanation
 
     def generate_html(self) -> str:
         # convert markdown list to html list, and linebreak to <br>.
@@ -59,9 +50,6 @@ class MistakeExplanationDocument(TensakuDocument):
     mistake_type: str
     explanation: str
     japanese: bool
-
-    def generate_md(self):
-        return self.explanation
 
     def generate_html(self) -> str:
         # convert markdown list to html list, and linebreak to <br>.
@@ -105,29 +93,6 @@ class SentenceExplanationDocument(TensakuDocument):
                         <p class='body-text'><span class='corrected'>{self.edited_sentence}</span></p>{mistake_explanations_html}
                         {complement_comment_html}
                     </div>"""
-                    
-    def generate_md(self):
-        if self.complement_comment:
-            return self.generate_md_with_complement()
-        else:
-            return self.generate_md_without_complement()
-    
-    def generate_md_without_complement(self, separator="\n\n"):
-        mistake_explanations_md = ""
-        for mistake_explanation in self.mistake_explanations:
-            mistake_explanations_md += mistake_explanation.generate_md()
-            mistake_explanations_md += separator
-        
-        return f"{self.original_sentence}\n\n{self.edited_sentence}\n\n{mistake_explanations_md}"
-
-    def generate_md_with_complement(self, separator="\n\n"):
-        mistake_explanations_md = ""
-        for mistake_explanation in self.mistake_explanations:
-            mistake_explanations_md += mistake_explanation.generate_md()
-            mistake_explanations_md += separator
-        self.complement_comment
-        
-        return f"{self.original_sentence}\n\n{self.edited_sentence}\n\n{mistake_explanations_md}\n\n{self.complement_comment}"
 
 
 
@@ -190,28 +155,6 @@ class MultipleChoiceQuizDocument(TensakuDocument):
             }}"""
         return check_answers_js
 
-    
-    def generate_md(self) -> str:
-        choices_md = ""
-        for choice in self.choices:
-            choices_md += f"- {choice}"
-        questions_md = ""
-        for question in self.questions:
-            questions_md += f"- {question}"
-        answers_md = ""
-        for answer in self.answers:
-            answers_md += f"- {answer}"
-        return f"""## {self.title}
-{self.mistake_document.generate_md()}
-### 選択肢
-{choices_md}
-
-### 問題
-{questions_md}
-
-## 回答
-{answers_md}
-"""
 
 
 @dataclass
@@ -240,8 +183,6 @@ class FreeAnswerQuizDocument(TensakuDocument):
         
         return cls(title, questions, answers)
     
-    def generate_md(self) -> str:
-        return super().generate_md()
     
     def generate_html(self):
         questions_html = ""
@@ -272,9 +213,6 @@ class FreeAnswerQuizDocument(TensakuDocument):
 @dataclass
 class QuizzesDocument(TensakuDocument):
     quizzes: list[FreeAnswerQuizDocument | MultipleChoiceQuizDocument]
-    
-    def generate_md(self) -> str:
-        return super().generate_md()
     
     def generate_html(self) -> str:
         return
@@ -500,88 +438,18 @@ class AllTensakuDocument(TensakuDocument):
     comment: str
     sentence_wise_explanations: List[SentenceExplanationDocument]
     quizzes: QuizzesDocument
-    VERSION:str = "0.0.1"
+    VERSION: str = "0.0.1"
     
+    def generate_dict(self) -> dict:
+        return {
+            "version": self.VERSION,
+            "original_paragraph": self.original_paragraph,
+            "edited_paragraph": self.edited_paragraph,
+            "native_paragraph": self.native_paragraph,
+            "native_explanation": self.native_explanation.explanation,
+            "comment": self.comment,
+            "sentence_wise_explanations": [sentence_explanation.__dict__ for sentence_explanation in self.sentence_wise_explanations],
+            "quizzes": self.quizzes,
+        }
     
-    def _generate_sentence_wise_explanations_md(self, separator="\n\n---\n"):
-        md = ""
-        for sentence_explanation in self.sentence_wise_explanations:
-            md += sentence_explanation.generate_md()
-            md += separator
-        return md
-        
-    def generate_md(self):
-        if self.japanese:
-            return f"""## あなたの英語
-{self.original_paragraph}
-
-## 添削
-{self.edited_paragraph}
-
-## ネイティブが使う表現
-{self.native_paragraph}
-
-## 文ごとの添削
-{self._generate_sentence_wise_explanations_md()}
-
-## Nicky からのコメント 
-{self.comment}
-"""
-        else:
-            return f"""## Your essay
-{self.original_paragraph}
-
-## Revised essay 
-{self.edited_paragraph}
-
-## Native essay
-{self.native_paragraph}
-
-## Sentence Wise Explanations
-{self._generate_sentence_wise_explanations_md()}
-
-## Comment from Nicky
-{self.comment}
-"""
-    def generate_html(self):
-        sentence_wise_explanations_html = "".join([sentence_explanation.generate_html() for sentence_explanation in self.sentence_wise_explanations])
-        #add version on the buttom
-        return f"""<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Tensaku Result</title>
-        <style>{style_text}</style>
-        <script>
-            function checkAnswers() {{
-                let correctCount = 0;
-
-                {self.quizzes.generate_js()}
-
-                document.getElementById('score').innerHTML = `You got ${{correctCount}} out of {len(self.quizzes.quizzes)} correct!`;
-            }}
-
-        // ... (rest of the JavaScript code)
-    </script>
-    </head>
-    <body>
-        <div class='container'>
-            <h2 class='title'>あなたの英語</h2>
-            <p class='body-text'>{self.original_paragraph}</p>
-            <h2 class='title'>添削</h2>
-            <p class='body-text'>{self.edited_paragraph}</p>
-            <h2 class='title'>ネイティブが使う表現</h2>
-            <p class='body-text'>{self.native_paragraph}</p>
-            <h2 class='title'>文ごとの添削</h2>
-            {sentence_wise_explanations_html}
-            <h2 class='title'>Nicky からのコメント</h2>
-            <p class='body-text'>{self.comment}</p>
-        </div>
-        <div class='container'>
-            {self.quizzes.generate_html()}
-        </div>
-        Version: {self.VERSION}
-    </body>
-</html>"""
-
 
