bf17c3ac707bbd9f02a5ebde8ddbed885c866c05
shge <25113178+shge@users.noreply.github.com>
2023-09-08 00:22:53 +0900

    Fix 'NoneType' object has no attribute 'paragraph'

--- tensaku/tensaku_generator.py
+++ tensaku/tensaku_generator.py
@@ -84,8 +84,8 @@ class TensakuGenerator():
 
         all_tensaku_document = AllTensakuDocument(
             original_paragraph = essay.paragraph,
-            edited_paragraph = corrected_essay.paragraph,
-            native_paragraph = native_example.paragraph,
+            edited_paragraph = corrected_essay.paragraph if corrected_essay != None else "",
+            native_paragraph = native_example.paragraph if native_example != None else "",
             native_explanation=native_explanation,
             comment = comment,
             sentence_wise_explanations = all_explanations,
