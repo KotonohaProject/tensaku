755e9d23bdef23e5ebac6e56fd96f4beb4461c00
ShinnosukeUesaka <shinnosuke.uesaka@gmail.com>
2023-10-16 18:37:45 -0700

    add azure test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -2,31 +2,40 @@ from tensaku.tensaku_generator import TensakuGenerator
 import json
 import importlib
 from dataclasses import asdict
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 import openai
 import time
+import os
 
 def test_generator():
     tensaku = TensakuGenerator()
     input_text =  'I looked a movie with my friends. It was very fun.'
-    
-    load_dotenv(".env.openai", override=True)
-    importlib.reload(openai)
-    
+
+    env_values = dotenv_values("tests/.env.openai")
+    openai.api_type = "open_ai"
+    openai.api_key = env_values['OPENAI_API_KEY']
+
+    assert openai.api_type == "open_ai"
+
     start_time_openai = time.time()
     all_tensaku_document_openai = tensaku.generate(input_text, generate_quiz=False)
     end_time_openai = time.time()
     openai_duration = end_time_openai - start_time_openai
-    
+
     # Time measurement for Azure
-    load_dotenv(".env.azure", override=True)
-    importlib.reload(openai)
-    
+    env_values = dotenv_values("tests/.env.azure")
+    openai.api_type = "azure"
+    openai.api_key = env_values['OPENAI_API_KEY']
+    openai.api_base = env_values['OPENAI_API_BASE']
+    openai.api_version = env_values['OPENAI_API_VERSION']
+
+    assert openai.api_type == "azure"
+
     start_time_azure = time.time()
     all_tensaku_document_azure = tensaku.generate(input_text, generate_quiz=False)
     end_time_azure = time.time()
     azure_duration = end_time_azure - start_time_azure
-    
+
     # Show the time results
     print(f"Time taken with OpenAI: {openai_duration} seconds")
     print(f"Time taken with Azure: {azure_duration} seconds")
